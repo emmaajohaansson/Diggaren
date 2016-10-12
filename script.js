72816a5b77913749deeacd513a945d987be2de4c
emmaajohaansson <emmakristina95@hotmail.com>
2016-10-12 12:04:54 +0200

    Hämtat från API

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-	get("http://api.sr.se/api/v2/channels/164"), function(response){}
-});
+$.ajax({
+                type: "GET",               
+               url: "http://api.sr.se/api/v2/channels/164",               
+                dataType: "xml",
+               error: function (response) {           
+                        alert('Error: There was a problem processing your request, please refresh the browser and try again');
+                },
+                success: function (response) {
+            console.log(response);
+               }
+        });
