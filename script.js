4c9512347f6159e101b11e542bec9a30a917f5d4
RobinJohannesson <robinjohannesson96@gmail.com>
2016-10-14 00:05:22 +0200

    kan inte hitta specifik låt
    men kan skriva in manuellt låten i url:en, men kan inte skriva in en
    variable från sr till spotifys url

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$.ajax({
+    $.ajax({
                 type: "GET",               
                url: "http://api.sr.se/api/v2/channels/164",               
                 dataType: "xml",
@@ -10,14 +10,34 @@ $.ajax({
                }
         });
 
-$.ajax({//https://developer.spotify.com/web-api/console/get-search-item/?q=eminem&type=track&market=US#complete
+    $.ajax({
                 type: "GET",               
-               url: "https://api.spotify.com/v1/search?q=till+i+collapse&type=track&market=SE&limit=1",
+               url: "http://api.sr.se/api/v2/playlists/rightnow?channelid=164",               
+                dataType: "xml",
+               error: function (response) {           
+                        alert('Error: There was a problem processing your request, please refresh the browser and try again');
+                },
+                success: function (response) {
+            console.log(response);
+                window.$title = $(response).find( "title" );
+            $( "#song_title" ).append( $title.text() );
+                $artist = $(response).find( "artist" );
+            $( "#song_artist" ).append( $artist.text() );
+            url_song = "https://api.spotify.com/v1/search?q=" + $title.text() + "&type=track,artist,album&market=SE&limit=1";
+            window['url_song'] = url_song;
+            console.log(url_song);
+               }
+        });
+
+    $.ajax({
+                type: "GET",               
+               url: url_song,
                 dataType: "json",
                error: function (response) {           
                         alert('Error: There was a problem processing your request, please refresh the browser and try again');
                 },
                 success: function (response) {
             console.log(response);
+            console.log($url_song);
                }
         }); 
