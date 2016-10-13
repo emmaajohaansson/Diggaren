1e3fd242fa9ee9e2120762bf430efb951abd0f29
RobinJohannesson <robinjohannesson96@gmail.com>
2016-10-13 21:45:17 +0200

    lagt till hur man söker efter en låt
    vet dock inte hur man får fram spotify resultatet

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ $.ajax({
             console.log(response);
                }
         });
+
+$.ajax({//https://developer.spotify.com/web-api/console/get-search-item/?q=eminem&type=track&market=US#complete
+                type: "GET",               
+               url: "https://api.spotify.com/v1/search?q=till+i+collapse&type=track&market=SE&limit=1",
+                dataType: "json",
+               error: function (response) {           
+                        alert('Error: There was a problem processing your request, please refresh the browser and try again');
+                },
+                success: function (response) {
+            console.log(response);
+               }
+        }); 
