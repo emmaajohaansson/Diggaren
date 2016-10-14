dcb20f28d2751e676812ea6720b5b62dfa9d83c6
RobinJohannesson <robinjohannesson96@gmail.com>
2016-10-14 10:48:50 +0200

    lagt till funktioner

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-    $.ajax({
+function sr_audio_song(){
+        $.ajax({
                 type: "GET",               
                url: "http://api.sr.se/api/v2/channels/164",               
                 dataType: "xml",
@@ -7,10 +8,14 @@
                 },
                 success: function (response) {
             console.log(response);
+                    $song = $(response).find( "url" );
+                    $( "#song_player" ).append( $song.text() );
                }
         });
+    };
 
-    $.ajax({
+    function sr_songs(){
+        $.ajax({
                 type: "GET",               
                url: "http://api.sr.se/api/v2/playlists/rightnow?channelid=164",               
                 dataType: "xml",
@@ -19,25 +24,28 @@
                 },
                 success: function (response) {
             console.log(response);
-                window.$title = $(response).find( "title" );
-            $( "#song_title" ).append( $title.text() );
-                $artist = $(response).find( "artist" );
-            $( "#song_artist" ).append( $artist.text() );
-            url_song = "https://api.spotify.com/v1/search?q=" + $title.text() + "&type=track,artist,album&market=SE&limit=1";
-            window['url_song'] = url_song;
-            console.log(url_song);
+                title = $(response).find( "title" );
+            $( "#song_title" ).append( title.text() );
+                artist = $(response).find( "artist" );
+            $( "#song_artist" ).append( artist.text() );
                }
         });
+    };
 
-    $.ajax({
+    function spotify_song(title){
+        $.ajax({
                 type: "GET",               
-               url: url_song,
+               url: "https://api.spotify.com/v1/search?q="+title+"&type=track,artist,album&market=SE&limit=1",
                 dataType: "json",
                error: function (response) {           
                         alert('Error: There was a problem processing your request, please refresh the browser and try again');
                 },
                 success: function (response) {
             console.log(response);
-            console.log($url_song);
                }
         }); 
+    };
+
+sr_songs()
+sr_audio_song()
+spotify_song(eminem)
