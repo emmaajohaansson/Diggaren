2c31bc1ab3654914cda0f5a4e7d76224f21e5a5f
RobinJohannesson <robinjohannesson96@gmail.com>
2016-10-15 15:01:45 +0200

    hittar låten från tidigare spelat på spotify
    men spammar man texten kommer en hel del iframes upp xD, kanske ska
    endast visa en och dölja när en annan visas eller något?

--- resources/js/script.js
+++ resources/js/script.js
@@ -54,12 +54,8 @@ function spotify_song(title) {
         },
         success: function (response) {
             console.log(response);
-            spotify_song_url = response.tracks.items[0].external_urls.spotify;
-            $("#spotify_song_url").append(spotify_song_url);
-            console.log(spotify_song_url);
             spotify_song_uri = response.tracks.items[0].uri;
             document.getElementById("spotify_song_uri").src = "https://embed.spotify.com/?uri=" + spotify_song_uri;
-            console.log(spotify_song_uri);
         }
     });
 };
@@ -91,7 +87,9 @@ function sr_audio_old_songs() {
             var list= "";
             var x = response.getElementsByTagName("song");
             for (i = 0; i <x.length; i++){
-                list += "<li onclick=spotify_old_song(" + x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + ") id=" + x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + ">" + x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue + "</li>";
+                song_name = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+                song_name_new = song_name.split(" ").join("+");
+                list += "<li onclick=spotify_old_song(this.id) id=" + song_name_new + ">" + song_name + "</li>";
             }
             document.getElementById("old_songs_title").innerHTML = list;
         }
@@ -99,25 +97,32 @@ function sr_audio_old_songs() {
 };
 
 function spotify_old_song(song_name) {
-    console.log(song_name);
-    spotify_song_url_append(song_name);
-    spotify_old_song_url = "<iframe src=https://embed.spotify.com/?uri=spotify%3Atrack%3A" + spotify_song_url + " width=300 height=80 frameborder=0 allowtransparency=true></iframe>";
-    console.log(spotify_old_song_url);
-    $(song_name).append(spotify_old_song_url);
+    song_name_new = song_name.split("+").join(" ");
+    spotify_song_url_append(song_name_new);
 }
 
 function spotify_song_url_append(title) {
     $.ajax({
         type: "GET",
-        url: "https://api.spotify.com/v1/search?q=eminem&type=track,artist,album&market=SE&limit=1",
+        url: "https://api.spotify.com/v1/search?q=" + title + "&type=track,artist,album&market=SE&limit=1",
         dataType: "json",
         error: function (response) {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
             spotify_song_url = response.tracks.items[0].uri;
-            console.log(spotify_song_url)
-            return spotify_song_url;
+            console.log(spotify_song_url);
+            spotify_old_song_url = "https://embed.spotify.com/?uri=" + spotify_song_url;
+            title_new = title.split(" ").join("+");
+            console.log(title_new)
+            console.log(spotify_old_song_url);
+            iframe = document.createElement("iframe");
+            console.log(iframe);
+            iframe.frameBorder=0;
+            iframe.width="300px";
+            iframe.height="80px";
+            iframe.setAttribute("src", spotify_old_song_url);
+            document.getElementById(title_new).appendChild(iframe);
         }
     });
 };
