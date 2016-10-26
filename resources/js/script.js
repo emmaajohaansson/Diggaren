43da14ecb10daa32719832954c92382f7f3de9b1
RobinJohannesson <robinjohannesson96@gmail.com>
2016-10-26 12:31:28 +0200

    ändrat variabelnamn

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,4 @@
-function sr_audio_song() {
+function sr_p3() {
     $.ajax({
         type: "GET",
         url: "http://api.sr.se/api/v2/channels/164",
@@ -7,25 +7,23 @@ function sr_audio_song() {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
-            console.log(response);
             find_picture = $(response).find("image");
 			console.log(find_picture.text());
 			sr_picture = document.createElement("img");
 			sr_picture.setAttribute("src", find_picture.text());
 			$("#sr_picture").append(sr_picture);
-            song = $(response).find("url");
-            console.log(song.text());
-            song_player_media(song.text())
+            radio_url = $(response).find("url");
+            sr_radio_player(radio_url.text());
         }
     });
 };
 
-function song_player_media(song) {
-    radio_player = "<audio controls=controls><source src=" + song + " type=audio/ogg /><source src=" + song + " type=audio/mpeg /></audio>";
-    $("#radio_player").append(radio_player);
+function sr_radio_player(radio_url) {
+    radio_player = "<audio controls=controls><source src=" + radio_url + " type=audio/ogg /><source src=" + radio_url + " type=audio/mpeg /></audio>";
+    $("#radio_player_container").append(radio_player);
 };
 
-function sr_songs() {
+function sr_current_song() {
     $.ajax({
         type: "GET",
         url: "http://api.sr.se/api/v2/playlists/rightnow?channelid=164",
@@ -34,7 +32,6 @@ function sr_songs() {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
-            console.log(response);
             title = $(response).find("song title");
             if (title.text() == "") {
                 $("#song_title").append("Ingen låt spelas just nu");
@@ -43,14 +40,14 @@ function sr_songs() {
                 $("#song_title").append(title.text());
                 artist = $(response).find("song artist");
                 $("#song_artist").append(artist.text());
-                spotify_song(title.text(), artist.text());
-                console.log(title.text() + " " + artist.text());
+                spotify_current_song(title.text(), artist.text());
             }
+
         }
     });
 };
 
-function spotify_song(title, artist) {
+function spotify_current_song(title, artist) {
     $.ajax({
         type: "GET",
         url: "https://api.spotify.com/v1/search?q=track:" + title + "%20artist:" + artist + "&type=track,artist,album&market=SE&limit=1",
@@ -59,21 +56,19 @@ function spotify_song(title, artist) {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
-            console.log(response);
-            spotify_song_url = response.tracks.items[0].external_urls.spotify;
             spotify_song_uri = response.tracks.items[0].uri;
-            spotify_song_uri_new = "https://embed.spotify.com/?uri=" + spotify_song_uri;
+            spotify_song_embed = "https://embed.spotify.com/?uri=" + spotify_song_uri;
             iframe = document.createElement("iframe");
             iframe.frameBorder=0;
             iframe.width="300px";
             iframe.height="80px";
-            iframe.setAttribute("src", spotify_song_uri_new);
+            iframe.setAttribute("src", spotify_song_embed);
             document.getElementById("spotify_current_song").appendChild(iframe);
         }
     });
 };
 
-function sr_audio_old_songs() {
+function sr_previous_songs() {
     $.ajax({
         type: "GET",
         url: "http://api.sr.se/api/v2/playlists/getplaylistbychannelid?id=164&size=20",
@@ -82,44 +77,36 @@ function sr_audio_old_songs() {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
         },
         success: function (response) {
-            console.log(response);
             var i;
             var list= "";
             var x = response.getElementsByTagName("song");
             for (i = 0; i <x.length; i++){
                 list = "";
-                song_name = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
+                previous_title = x[i].getElementsByTagName("title")[0].childNodes[0].nodeValue;
                 start_time_song = x[i].getElementsByTagName("starttimeutc")[0].childNodes[0].nodeValue;
-                artist_song = x[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue;
-                start_time_song_correction = start_time_song.substring(11, 13);
-                start_time_song_correction =+ start_time_song_correction + 2;
-                start_time_song_new = start_time_song.substring(13, 16);
-                start_time_song_correction += start_time_song_new;
-                song_name_new = song_name.split(" ").join("+");
-                list += "<li onclick=spotify_old_song(this.id) id=&quot;" + song_name_new + "&quot;>" + start_time_song_correction + " - &quot;" + song_name + "&quot; av " + artist_song + "</li>";
-                $("#old_songs_title").append(list);
-                song_name_new = song_name.split("+").join(" ");
-                console.log(list);
-                spotify_song_url_append(song_name, artist_song);
-                function spotify_song_url_append_callback(results){
-                       console.log(results);
-                };  
-                console.log(test_return);
+                previous_artist = x[i].getElementsByTagName("artist")[0].childNodes[0].nodeValue;
+                start_time_song_hour = start_time_song.substring(11, 13);
+                start_time_song_hour =+ start_time_song_hour + 2;
+                start_time_song_minute = start_time_song.substring(13, 16);
+                start_time_song_hour += start_time_song_minute;
+                previous_title_id = previous_title.split(" ").join("+");
+                previous_artist_id = previous_artist.split(" ").join("+");
+                previous_title_artist_id = previous_title_id + "¤" + previous_artist_id;
+                list += "<li onclick=spotify_previous_song_player(this.id) id=&quot;" + previous_title_artist_id + "&quot;>" + start_time_song_hour + " - &quot;" + previous_title + "&quot; av " + previous_artist + "</li>";
+                $("#previous_songs_title").append(list);
             }
-            console.log(list);
         }
     });
 };
 
-function spotify_old_song(song_name) {
-    song_name_new = song_name.split("+").join(" ");
-    spotify_song_url_append(song_name_new);
-};
-
-function spotify_song_url_append(title, artist) {
+function spotify_previous_song_player(title) {
+    previous_title = title.split("+").join(" ");
+    previous_title_artist_id = title.split("¤").join(" ");
+    console.log(title)
+    console.log(previous_title_artist_id)
     $.ajax({
         type: "GET",
-        url: "https://api.spotify.com/v1/search?q=track:" + title + "%20artist:" + artist + "&type=track,artist,album&market=SE&limit=1",
+        url: "https://api.spotify.com/v1/search?q=" + previous_title + "&type=track,artist,album&market=SE&limit=1",
         dataType: "json",
         error: function (response) {
             alert('Error: There was a problem processing your request, please refresh the browser and try again');
@@ -132,12 +119,10 @@ function spotify_song_url_append(title, artist) {
             iframe.width="300px";
             iframe.height="80px";
             iframe.setAttribute("src", spotify_old_song_url);
-            document.getElementById("old_songs_title").appendChild(iframe);
-            spotify_song_url_append_callback(iframe);
+            document.getElementById(title).appendChild(iframe);
         }
     });
 };
 
-sr_songs()
-sr_audio_song()
-sr_audio_old_songs()
+sr_p3()
+sr_previous_songs()
